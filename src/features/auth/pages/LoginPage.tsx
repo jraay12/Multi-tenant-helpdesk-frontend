59bcd1fcdd4f4a71847360b1c0c6329c489c483b
John Ray <johnraycaete@gmail.com>
2026-05-13 12:35:43 +0800

    added the login page

---
+++ src/features/auth/pages/LoginPage.tsx
@@ -0,0 +1,109 @@
+import { useForm } from "react-hook-form";
+import type { LoginUserInput } from "./types";
+import { Lock } from "lucide-react";
+import { EyeClosed, Eye } from "lucide-react";
+const LoginPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (data: LoginUserInput) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="flex justify-center items-center h-screen bg-[#e1eafa]">
+      <section className="w-full max-w-md bg-white rounded-2xl shadow-lg p-8">
+        <h1 className="text-xl font-bold text-center mt-2">
+          Sign in to WorksyHub
+        </h1>
+
+        <p className="text-sm text-gray-500 text-center mt-2 mb-6">
+          Enter your credentials to access your workspace and support tools
+        </p>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+          {/* Email */}
+          <div className="flex flex-col gap-1">
+            <label className="text-sm font-medium">Email address</label>
+
+            <div className="relative w-full">
+              {/* Prefix */}
+              <div className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400">
+                @
+              </div>
+
+              <input
+                type="email"
+                placeholder="name@company.com"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "Invalid email format",
+                  },
+                })}
+                className="border border-gray-200 rounded-lg pl-8 pr-3 py-2 w-full outline-none focus:ring-1 focus:ring-gray-200"
+              />
+            </div>
+
+            {errors.email && (
+              <span className="text-red-500 text-xs">
+                {errors.email.message}
+              </span>
+            )}
+          </div>
+
+          {/* Password */}
+          <div className="flex flex-col gap-1">
+            <div className="flex justify-between">
+              <label className="text-sm font-medium">Password</label>
+              <p className="text-sm text-blue-600 font-medium cursor-pointer">Forgot Password?</p>
+            </div>
+
+            <div className="relative w-full">
+              <Lock className="absolute left-3 top-1/2 -translate-y-1/2 w-4 h-4 text-black" />
+
+              <input
+                type="password"
+                placeholder="••••••••"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters",
+                  },
+                })}
+                className="border border-gray-200 rounded-lg pl-9 pr-3 py-2 w-full outline-none focus:ring-1 focus:ring-gray-200"
+              />
+              <Eye className="absolute -translate-y-1/2 right-3 top-1/2"/>
+            </div>
+
+            {errors.password && (
+              <span className="text-black text-xs">
+                {errors.password.message}
+              </span>
+            )}
+          </div>
+
+          <button
+            type="submit"
+            disabled={isSubmitting}
+            className="bg-blue-600 text-white py-2 rounded-lg font-medium hover:bg-blue-700 transition disabled:opacity-50"
+          >
+            {isSubmitting ? "Signing in..." : "Sign In"}
+          </button>
+        </form>
+      </section>
+    </main>
+  );
+};
+
+export default LoginPage;
