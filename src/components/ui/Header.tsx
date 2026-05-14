5db2cf1f62fe5195a8a548534e7253d8bd0ae184
John Ray <johnraycaete@gmail.com>
2026-05-14 14:49:36 +0800

    feat: add header component

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,51 +1,52 @@
-import { useState } from "react";
-import { Bell, User2 } from "lucide-react";
+import { Bell, User2, HelpCircleIcon, InfoIcon } from "lucide-react";
 import Button from "./Button";
+import SearchComponent from "./Search";
+import { useLocation } from "react-router";
 
 const Header = () => {
-  const [activeTab, setActiveTab] = useState("all");
-
-  const tabs = [
-    { label: "All Tickets", value: "all" },
-    { label: "My Tasks", value: "my" },
-    { label: "Unassigned", value: "unassigned" },
-  ];
-
+  const location = useLocation();
+  
   return (
     <div className="border-b border-gray-300 px-4 py-2 flex items-center justify-between">
-      
       {/* Left */}
-      <div className="flex gap-6 items-center">
-        <h1 className="font-bold">Workspace</h1>
-
-        {/* Tabs */}
-        {tabs.map((tab) => (
-          <button
-            key={tab.value}
-            onClick={() => setActiveTab(tab.value)}
-            className={`text-sm font-medium transition cursor-pointer ${
-              activeTab === tab.value
-                ? "border-b-2 border-blue-500 pb-1 text-blue-500"
-                : "text-gray-500"
-            }`}
-          >
-            {tab.label}
-          </button>
-        ))}
-      </div>
-
-      {/* Right */}
-      <div className="flex gap-2 items-center">
-        <div className="border border-gray-300 rounded-full cursor-pointer">
-          <Bell className="p-1 w-6 h-6" />
+      {location.pathname === "/workspace" && (
+        <div className="flex gap-6 items-center">
+          <h1 className="font-bold">ResolvDesk</h1>
         </div>
+      )}
 
-        <div className="border border-gray-300 rounded-full cursor-pointer">
-          <User2 className="p-1 w-6 h-6" />
+      {location.pathname === "/" && (
+        <>
+          <div className="w-1/3">
+            <SearchComponent placeholder="Search tickets, customer, or internat docs..." />
+          </div>
+
+          <div className="flex gap-5 items-center">
+            <Button button_name="Create Ticket" />
+            <div className="border border-gray-300 rounded-full cursor-pointer">
+              <Bell className="p-1 w-6 h-6" />
+            </div>
+
+            <div className="border border-gray-300 rounded-full cursor-pointer">
+              <InfoIcon className="p-1 w-6 h-6" />
+            </div>
+            <div className="border-l border-gray-400 w-4 h-10"></div>
+            <div className="flex items-center gap-4">
+              <User2 />
+              <div className="flex-col flex w-30 text-center">
+                <h1 className="text-x1s">Alex Rivera</h1>
+                <p className="text-xs">LEAD ARCHITECT</p>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+      {location.pathname === "/workspace" && (
+        <div className="flex items-center gap-3">
+          <HelpCircleIcon className="text-gray-500" />
+          <div className="h-7 w-7 cursor-pointer rounded-full bg-black"></div>
         </div>
-
-        <Button button_name="Create Ticket" />
-      </div>
+      )}
     </div>
   );
 };
