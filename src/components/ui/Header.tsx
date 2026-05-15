aabe6a681f3859fde47be5914cdbfab1736a5e2d
John Ray <johnraycaete@gmail.com>
2026-05-15 16:29:32 +0800

    update the UI if in ticket page details

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,56 +1,92 @@
-import { Bell, User2, HelpCircleIcon, InfoIcon } from "lucide-react";
+import { Bell, User2, HelpCircleIcon, InfoIcon, ArrowLeft } from "lucide-react";
 import Button from "./Button";
 import SearchComponent from "./Search";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate, useParams } from "react-router";
 import { useFetchMyWorkspaceById } from "../../features/workspace/hooks/useFetchMyWorkspaceById";
+import { useGetTicketById } from "../../features/tickets/hooks/useGetTicketsById";
 
 type HeaderProps = {
-  onClick: () => void
-}
+  onClick: () => void;
+};
+
 const Header = (props: HeaderProps) => {
   const location = useLocation();
-  const activeWorkspaceId = localStorage.getItem("workspace")
+  const navigate = useNavigate();
+  const { id } = useParams();
 
-  const {data} = useFetchMyWorkspaceById(activeWorkspaceId!)
+  const activeWorkspaceId = localStorage.getItem("workspace");
+  const { data: workspace } = useFetchMyWorkspaceById(activeWorkspaceId!);
+
+  const isWorkspacePage = location.pathname === "/workspace";
+
+  // only fetch when viewing ticket details
+  const { data: ticket } = useGetTicketById(id!);
+
+  const isTicketDetails = !!id;
+
   return (
     <div className="border-b border-gray-300 px-4 py-2 flex items-center justify-between">
-      {/* Left */}
-      {location.pathname === "/workspace" && (
+      {/* LEFT SIDE */}
+      {isWorkspacePage ? (
         <div className="flex gap-6 items-center">
           <h1 className="font-bold">ResolvDesk</h1>
         </div>
-      )}
+      ) : (
+        <div className="flex items-center gap-4 ">
+          {/* BACK BUTTON + TITLE (DETAIL PAGE) */}
+          {isTicketDetails ? (
+            <>
+              <button
+                onClick={() => navigate("/tickets")}
+                className="flex items-center gap-2 text-sm font-medium cursor-pointer text-black/40 hover:text-black/60"
+              >
+                <ArrowLeft className="w-4 h-4" />
+                Back to Tickets
+              </button>
+              <div className="border-r border-gray-400 w-2 h-10"></div>
 
-      {location.pathname !== "/workspace" && (
-        <>
-          <div className="w-1/3">
-            <SearchComponent placeholder="Search tickets, customer, or internat docs..." />
-          </div>
-
-          <div className="flex gap-5 items-center">
-            <Button button_name="Create Ticket" onClick={props.onClick} />
-            <div className="border border-gray-300 rounded-full cursor-pointer">
-              <Bell className="p-1 w-6 h-6" />
-            </div>
-
-            <div className="border border-gray-300 rounded-full cursor-pointer">
-              <InfoIcon className="p-1 w-6 h-6" />
-            </div>
-            <div className="border-l border-gray-400 w-4 h-10"></div>
-            <div className="flex items-center gap-4">
-              <User2 />
-              <div className="flex-col flex w-30 text-center font-medium">
-                <h1 className="text-x1s">Alex Rivera</h1>
-                <p className="text-xs ">{data?.role}</p>
+              <h1 className="text-lg font-semibold">
+                {ticket?.title ?? ""}
+              </h1>
+            </>
+          ) : (
+            <>
+              {/* SEARCH (LIST PAGE ONLY) */}
+              <div className="w-96">
+                <SearchComponent placeholder="Search tickets, customer, or internal docs..." />
               </div>
-            </div>
-          </div>
-        </>
+            </>
+          )}
+        </div>
       )}
-      {location.pathname === "/workspace" && (
+
+      {/* RIGHT SIDE */}
+      {isWorkspacePage ? (
         <div className="flex items-center gap-3">
           <HelpCircleIcon className="text-gray-500" />
           <div className="h-7 w-7 cursor-pointer rounded-full bg-black"></div>
+        </div>
+      ) : (
+        <div className="flex gap-5 items-center">
+          <Button button_name="Create Ticket" onClick={props.onClick} />
+
+          <div className="border border-gray-300 rounded-full cursor-pointer">
+            <Bell className="p-1 w-6 h-6" />
+          </div>
+
+          <div className="border border-gray-300 rounded-full cursor-pointer">
+            <InfoIcon className="p-1 w-6 h-6" />
+          </div>
+
+          <div className="border-l border-gray-400 w-4 h-10"></div>
+
+          <div className="flex items-center gap-4">
+            <User2 />
+            <div className="flex flex-col w-30 text-center font-medium">
+              <h1 className="text-xs">Alex Rivera</h1>
+              <p className="text-xs">{workspace?.role}</p>
+            </div>
+          </div>
         </div>
       )}
     </div>
