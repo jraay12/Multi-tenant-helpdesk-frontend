2fddf0011812e335dceb7f64ee97339fbf3dfb1b
John Ray <johnraycaete@gmail.com>
2026-05-15 11:50:52 +0800

    invalidate the cache query

--- src/features/tickets/hooks/useCreateTicket.ts
+++ src/features/tickets/hooks/useCreateTicket.ts
@@ -1,8 +1,13 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CreateTicket } from "../api/CreateTicket";
 
 export const useCreateTicket = () => {
+  const queryClient  = useQueryClient()
+  const workspaceId = localStorage.getItem("workspace")
   return useMutation({
     mutationFn: CreateTicket,
+    onSuccess: () => {
+      queryClient.invalidateQueries({queryKey: ['ticket', workspaceId]})
+    }
   });
 };
