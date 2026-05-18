c19c2282036b734d30e5b26ae8d7866919100311
John Ray <johnraycaete@gmail.com>
2026-05-18 10:29:51 +0800

    invalidate the recent ticket when new ticket created

--- src/features/tickets/hooks/useCreateTicket.ts
+++ src/features/tickets/hooks/useCreateTicket.ts
@@ -2,13 +2,18 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { CreateTicket } from "../api/CreateTicket";
 
 export const useCreateTicket = () => {
-  const queryClient  = useQueryClient()
-  const workspaceId = localStorage.getItem("workspace")
+  const queryClient = useQueryClient();
+  const workspaceId = localStorage.getItem("workspace");
   return useMutation({
     mutationFn: CreateTicket,
     onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ['ticket', workspaceId]})
-      queryClient.invalidateQueries({queryKey: ['dashboard-stats', workspaceId]})
-    }
+      queryClient.invalidateQueries({ queryKey: ["ticket", workspaceId] });
+      queryClient.invalidateQueries({
+        queryKey: ["dashboard-stats", workspaceId],
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["recent-ticket", workspaceId],
+      });
+    },
   });
 };
